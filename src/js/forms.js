33d4603b4f2d0498f0d4c7bef6a7ced2fdc5ecbe
Daniel <danieloliveiradesouza09@outlook.com>
2025-06-06 11:10:59 -0300

    corrigindo form

--- src/js/forms.js
+++ src/js/forms.js
@@ -1,13 +1,11 @@
-
-
 function update_status(){
     form_name = document.getElementById("name").value
     form_email = document.getElementById("email").value
     form_msg  = document.getElementById("message").value
-
     if (form_name.length == 0) 
         return
-    if (form_msg.length == 3) 
+    console.log(form_email)
+    if (form_email.length <= 3 && form_email.includes("@")) 
         return
     if (form_msg.length == 0) 
         return
